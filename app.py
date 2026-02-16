253fb944dfb630627d7b4d9359e47f7af30f79ba
SupremeGoogle <akbarchik0071@gmail.com>
2026-02-16 13:23:18 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-def handler(request):
-    return "Webhook is working"
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Server is running"
+
+@app.route("/s20-hook", methods=["POST"])
+def s20_hook():
+    data = request.json
+    print("Webhook received:", data)
+    return "OK", 200
